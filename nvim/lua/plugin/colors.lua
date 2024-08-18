c9a8927020efc462632f0034d731fb95ab93d836
pita <154020297+pita092@users.noreply.github.com>
2024-08-18 16:16:26 +0300

    Delete nvim directory

--- nvim/lua/plugin/colors.lua
+++
@@ -1,6 +0,0 @@
-return{
-	--{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	{"AlphaTechnolog/pywal.nvim", name = 'pywal'},
-{"norcalli/nvim-colorizer.lua"},
-"xiyaowong/transparent.nvim"
-}
